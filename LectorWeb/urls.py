083956fdd9a563af596216823e0fdab761bfe299
ProyectDongo <Dongo.proyectos@gmail.com>
2025-04-20 16:51:00 -0400

    si

--- LectorWeb/urls.py
+++ LectorWeb/urls.py
@@ -19,8 +19,8 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', views.home, name='home'),
+    path("home", admin.site.urls),
+    path("", views.home, name='home'),
     path('registrar_usuario/', views.registrar_usuario, name='registrar_usuario'),
 
     path('registro_huella/<int:user_id>/', views.registro_huella, name='registro_huella'),
